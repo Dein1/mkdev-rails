993b2eb7f7df28561c23cc447b697d3e5e66b424
Dmitry Abramov <dmitry@iMac.local>
2021-01-27 21:55:23 +0300

    create CRUD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,49 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.page(params[:page])
+    @events = Event.order(created_at: :desc).page(params[:page])
   end
 
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(**event_params, starttime: Time.now, endtime: Time.now + 1.day)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to events_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def event_params
+      params.require(:event).permit!
+    end
 end
