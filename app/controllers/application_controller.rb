61a0dd1ad94999e280236e62c76728666e35c6e9
Dmitry Abramov <dmitry@iMac.local>
2021-01-31 19:35:31 +0300

    add user model with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger, :warning
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name last_name email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name last_name email password current_password])
+  end
 end
